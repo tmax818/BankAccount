c1b1612b1aa75b4a320807500c028f5e0dee6c3d
tmax818 <tylermaxwell661@gmail.com>
2022-09-26 22:03:47 -0700

    sol w/o bonus

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,42 @@
 public class BankAccount {
+    double checkingBalance;
+    double savingsBalance;
+    static int NUMBER_OF_ACCOUNTS;
+    static double TOTAL_DEPOSITS;
+
+    public BankAccount(){
+        NUMBER_OF_ACCOUNTS++;
+    }
+
+    public void Deposit(String acctType, double amount){
+        if(acctType == "checking"){
+            this.checkingBalance += amount;
+            TOTAL_DEPOSITS += amount;
+        } else {
+            this.savingsBalance += amount;
+            TOTAL_DEPOSITS += amount;
+        }
+    }
+
+    public void Withdraw(String acctType, double amount){
+        if(acctType == "checking" && this.checkingBalance >= amount){
+            this.checkingBalance -= amount;
+            TOTAL_DEPOSITS -= amount;
+        } else if(acctType == "savings" && this.savingsBalance >= amount){
+            this.savingsBalance -= amount;
+            TOTAL_DEPOSITS -= amount;
+        }
+    }
+
+    public double TotalAmt(){
+        return checkingBalance + savingsBalance;
+    }
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
 }
